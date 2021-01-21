ba55960e107083e127a98df277b65ec8d597b9f5
park-chang-jin <49690383+park-chang-jin@users.noreply.github.com>
2021-01-21 12:24:42 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // navbar에 높이를 불러옴
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height; //getBoundingClientRect()은 브라우저에서 보여지는 width나 height를 가져옴, offsetWidth은 실제 넓이를 가져옴
-// scroll될 때마다 불러온다
+// scroll될 때마다 Y축 높이를 불러온다
 document.addEventListener("scroll", () => {
   console.log(window.scrollY);
   console.log(`navbarHeight: ${navbarHeight}`);
@@ -13,4 +13,18 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// ***Handle scrolling when tapping on the navbar menu ***
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  console.log(event.target.dataset.link); // html에 있는  data-link에 값이 나옴
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return; //link가 아닌 다른 부분 클릭 햇을때 반환
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" }); //scrollIntoView:해당 element로 이동, behavior: "smooth": 부드럽게 그 위치고 가게 해줌
 });
